399e7a164a71ebdb0ab652ce53d7d3faf30ee0dd
Romeo <romeogescolano@gmail.com>
2022-10-24 13:53:56 +0800

    fix: add: fix font, add animation to next buttons of sliders

--- script/script.js
+++ script/script.js
@@ -1,13 +1,29 @@
+var slideIndex_promotion = 1;
+showImgs(slideIndex_promotion);
+
+function plusIndex(m) {
+  showImgs(slideIndex_promotion += m);
+}
+
+function showImgs(m) {
+  var j;
+  var y = document.getElementsByClassName("promotion_slide");
+  if (m > y.length) {slideIndex_promotion = 1}
+  if (m < 1) {slideIndex_promotion = y.length}
+  for (j = 0; j < y.length; j++) {
+    y[j].style.display = "none";  
+  }
+  y[slideIndex_promotion-1].style.display = "block";  
+}
+
 var slideIndex = 1;
 showDivs(slideIndex);
-
 function plusDivs(n) {
   showDivs(slideIndex += n);
 }
-
 function showDivs(n) {
   var i;
-  var x = document.getElementsByClassName("promotion_slide");
+  var x = document.getElementsByClassName("Company_profile_slide");
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
