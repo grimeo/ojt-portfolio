321f1fd0a1e767d474862352fca3c095860b2a66
Romeo <romeogescolano@gmail.com>
2022-10-25 22:11:58 +0800

    fix: viewport width; added js script for changing viewport from desktop to mobile

--- script/script.js
+++ script/script.js
@@ -31,3 +31,10 @@ function showDivs(n) {
   }
   x[slideIndex-1].style.display = "block";  
 }
+
+var viewMode = getCookie("view-mode");
+if(viewMode == "desktop"){
+    viewport.setAttribute('content', 'width=1024');
+}else if (viewMode == "mobile"){
+    viewport.setAttribute('content', 'width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no');
+}
